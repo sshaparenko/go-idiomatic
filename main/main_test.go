55fd0a2c1dda1a13feedec1a41de5edc9aed324d
sshaparenko <zshaparenkoz@gmail.com>
2024-05-21 21:44:53 +0300

    failed test

--- main/main_test.go
+++ main/main_test.go
@@ -10,3 +10,12 @@ func TestAdd_Success(t *testing.T) {
 		t.Errorf("got %q, wanted %q", got, want)
 	}
 }
+
+func TestAdd_Fail(t *testing.T) {
+	got := Add(1, 2)
+	want := 4
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
